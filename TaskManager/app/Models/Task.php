59a25688974c1087514dc65c7061157470ce7db9
Vitor Garcia <vitorh_mariano06@hotmail.com>
2023-07-05 14:22:08 -0300

    Task creation

---
+++ TaskManager/app/Models/Task.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description'];
+}
+
