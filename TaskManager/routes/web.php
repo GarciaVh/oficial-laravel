694d624d85036c3aeeae36692d7065ed33761dcc
Vitor Garcia <vitorh_mariano06@hotmail.com>
2023-07-05 14:12:23 -0300

    first commit

---
+++ TaskManager/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Site\SiteController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/forum', [ForumController::class, 'index']) ->name ('forum.index');    
+
+Route::get('/contato', [SiteController::class, 'contact']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
